c627450338a58dc7ef22dd469a074184ce62f59a
rtestard <rtestard@opensense.fr>
2020-02-17 09:43:01 +0100

    I have solved reverseInt

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    if (typeof n !== "number") {
+        return
+    }
+
+    const nArray = n.toString().split('')
+
+    if (Math.sign(n) === -1) {
+        nArray.shift()
+        return (parseInt(nArray
+            .reverse().join(''))) * -1
+    } 
+
+    return parseInt(nArray.reverse().join(''))
+}
+
+console.log(reverseInt(-500))
+
+// Log Linear Time Complexity O(n log(n)) as we loop and sort n values
+// Linear Time complexity O(n) as we created a variable storing n values
+
 
 module.exports = reverseInt;
