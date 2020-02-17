1869176104b7ae79b1c056c4740fc4a8fc7d8ba2
rtestard <rtestard@opensense.fr>
2020-02-17 15:45:24 +0100

    I have added maxChar solution in javascript

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const dict = {}
+    let max = 0
+    let maxChar = ''
+
+    for (let l of str) {
+        if (dict[l]) {
+            dict[l] ++
+        } else {
+            dict[l] = 1
+        }
+    }
+
+    for (let char in dict) {
+        if (dict[char] > max) {
+            maxChar = char
+            max = dict[char]
+        }
+    }
+
+    return [maxChar, max]
+
+}
+
+console.log(maxChar(("abccccccd1111111111")))
 
 module.exports = maxChar;
