a105fe04ba20566d457b98aec2545104c2b440fb
rtestard <rtestard@opensense.fr>
2020-02-19 09:56:49 +0100

    I have develop anagrams algorithm

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,51 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/* function anagrams(stringA, stringB) {
+    function formatStr(str) {
+        return str.toLowerCase().replace(/[^\w]/g, '').split('').sort().join('')
+    }
+
+    const strA = formatStr(stringA)
+    const strB = formatStr(stringB)
+
+    if (strA === strB) {
+        return true
+    }
+    return false
+
+} */
+
+// Log Linear Time Complexity, O(n log(n)) as we loop and sort
+// Linear Space Complexity, O(n) as we create variables and store n values in them
+
+function anagrams(stringA, stringB) {
+
+    function handleDict(str) {
+        const dict = {}
+        for (let letter of str.replace(/[^\w]/g, '').toLowerCase()) {
+            dict[letter] = dict[letter] + 1 || 1;
+        }
+        return dict
+    }
+
+    const dictA = handleDict(stringA)
+    const dictB = handleDict(stringB)
+
+    for (let char in dictA) {
+        if(dictA[char] !== dictB[char]) {
+            return false
+        }
+    }
+
+    return true
+}
+
+// Linear Time Complexity, O(n), as we loop though n values
+// Linear Space Complexity, O(n), as we create variables storing n values
+
+
+
+console.log(anagrams('Hello', 'HellA'))
 
 module.exports = anagrams;
