82202ce60f4605ad1e05cae3adae087cf201a2fd
rtestard <rtestard@opensense.fr>
2020-02-20 09:58:09 +0100

    I have solved capitalize algorithms

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,38 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+/* function capitalize(str) {
+   
+    const words = [];
+    
+    for(let word of str.split(' ')) {
+        words.push(word[0].toUpperCase() + word.slice(1))
+    }
+
+    return words.join(' ')
+
+} */
+
+// Linear Time Complexity, O(n) as we loop though n values
+// Linear Space Complexity, O(n) as we create 1 variable storing n values
+
+function capitalize(str) {
+    let result = str[0].toUpperCase()
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase()
+        } else {
+            result += str[i]
+        }
+    }
+
+    return result
+}
+
+// Linear Time Complexity, O(n) as we loop though n values
+// Linear Space Complexity, O(n) as we create 1 variable storing n values
+
+console.log(capitalize('look, it is working!'))
 
 module.exports = capitalize;
