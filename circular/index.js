3140eded82fed7339a99fd5401a95e9e559b19e0
rtestard <rtestard@opensense.fr>
2020-03-02 09:40:47 +0100

    I have added an algo on linkedlist

--- circular/index.js
+++ circular/index.js
@@ -10,8 +10,35 @@
 //   a.next = b;
 //   b.next = c;
 //   c.next = b;
-//   circular(l) // true
+//    // true
 
-function circular(list) {}
+const L = require('./linkedlist');
+const List = L.LinkedList;
+const Node = L.Node;
+
+function circular(list) {
+    let slow = list.head;
+    let fast = list.head;
+
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            return true
+        }
+    }
+    return false
+}
+
+const l = new List();
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+l.head = a;
+a.next = b;
+b.next = c;
+c.next = b;
+console.log(circular(l))
 
 module.exports = circular;
